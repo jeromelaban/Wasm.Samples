621c3789063c36d2c744df9b89d92eb69cafef0f
Jérôme Laban <jerome.laban@nventive.com>
2018-06-08 11:56:23 -0400

    Added JSon.NET [JsonConstructor] sample.

---
+++ Json.net.Tests/JsonNet.Testing/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonNet.Testing
+{
+    public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var test = JsonConvert.DeserializeObject<MyTest>("{ 'myValue': 'test' }");
+
+			Console.WriteLine($"MyValue = {test.MyValue2}");
+		}
+	}
+
+	public class MyTest
+	{
+		public MyTest()
+		{
+
+		}
+
+		[JsonConstructor]
+		public MyTest(string myValue)
+		{
+			MyValue2 = myValue;
+		}
+
+		public string MyValue2 { get; set; }
+	}
+}
