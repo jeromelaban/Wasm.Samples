c14d451be5e7b43a5e45998973acdb9ffd43162a
Jérôme Laban <jerome.laban@nventive.com>
2019-03-14 12:03:48 -0400

    Add mixed mode test for mono issue

---
+++ MixedMode.Tests/MixedModeTests/Program.cs
@@ -0,0 +1,35 @@
+﻿using MyLibrary;
+using System;
+using System.Threading;
+using Uno.Extensions;
+using Windows.Foundation.Collections;
+
+namespace WasmTimerTests
+{
+    public class Class1
+    {
+        static event VectorChangedEventHandler<object> VectorChanged;
+
+        static void Main(string[] args)
+        {
+            VectorChangedEventHandler<object> onVectorChanged =
+                (o, e) => OnItemsSourceSingleCollectionChanged(o, e.ToNotifyCollectionChangedEventArgs());
+
+            VectorChanged += onVectorChanged;
+
+            VectorChanged?.Invoke(null, new MyArgs());
+        }
+
+        private static void OnItemsSourceSingleCollectionChanged(object o, object p)
+        {
+            Console.WriteLine("OnItemsSourceSingleCollectionChanged");
+        }
+
+        private class MyArgs : IVectorChangedEventArgs
+        {
+            public CollectionChange CollectionChange => CollectionChange.ItemChanged;
+
+            public uint Index => 0;
+        }
+    }
+}
---
+++ MixedMode.Tests/MyLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public static class Class1
+    {
+        public static object MyExtension(this object instance) => instance;
+    }
+}
