95038413b7355283a4d27972fb34d14fce1645eb
Jérôme Laban <jerome.laban@nventive.com>
2019-03-14 22:18:29 -0400

    Repro for issue https://github.com/mono/mono/issues/12981

---
+++ Bug12981-InterpRecursion/BugInterpRecursion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace WasmTimerTests
+{
+    public class Class1
+    {
+        static void Main(string[] args) => MethodA(null);
+
+        internal static bool MethodA(object sender)
+        {
+            if (sender == null) { return true; }
+            return MethodA(sender);
+        }
+    }
+}
