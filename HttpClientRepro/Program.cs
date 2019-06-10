e7b26fd902e3652928ec79172c15b9c298e41d0f
Jerome Laban <jerome.laban@nventive.com>
2019-06-10 09:34:56 -0400

    Add HttpClient AOT Repro

---
+++ HttpClientRepro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HttpClientRepro
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod();
+        }
+
+        private static async Task NewMethod()
+        {
+            await Fetch();
+            await Fetch();
+        }
+
+        private static async Task Fetch()
+        {
+            try
+            {
+                var client = new System.Net.Http.HttpClient()
+                {
+                    DefaultRequestHeaders = { { "origin", "WindowsCalculator" } }
+                };
+
+                var r = await client.GetStringAsync(new Uri("https://cors-anywhere.herokuapp.com/https://go.microsoft.com/fwlink/?linkid=2041093&localizeFor=en-US"));
+
+                Console.WriteLine(r);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
