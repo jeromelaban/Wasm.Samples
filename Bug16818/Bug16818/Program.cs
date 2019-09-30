88f7179986bcd53c2ce171f5f2a5b1f5093fcf39
Jerome Laban <jerome.laban@nventive.com>
2019-09-30 11:34:34 -0400

    Bug 16818 repro

---
+++ Bug16818/Bug16818/Program.cs
@@ -0,0 +1,17 @@
+﻿using Bug16818;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WasmTimerTests
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("s1");
+            var logger = new LoggerFactory();
+            Console.WriteLine("s2");
+        }
+    }
+}
---
+++ Bug16818/OtherLibrary/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bug16818
+{
+    public class LoggerFactory
+    {
+        private IDisposable[] _providers = (IDisposable[])new IDisposable[0];
+
+        private readonly object _sync = new object();
+    }
+}
