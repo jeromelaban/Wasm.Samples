01dd6907b721f607d2e9f92ec08d2fc3d9136e3e
Jerome Laban <jerome.laban@nventive.com>
2020-09-09 14:56:04 -0400

    Add basic AOT test validation

---
+++ BasicAOTTest/BasicAOTTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace WasmTimerTests
+{
+    public class Class1
+    {
+
+        static void Main(string[] args)
+        {
+			Action<int> a = i => { Console.WriteLine("test"); };
+        }
+    }
+}
