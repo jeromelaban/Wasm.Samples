652f30501e55bfdad84342659f405a4cb1f55fed
Jerome Laban <jerome.laban@nventive.com>
2020-11-24 12:33:06 -0500

    Add repro for https://github.com/dotnet/runtime/issues/44270

---
+++ Bug44270/Bug44270/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+InternalImportTest.mono_trace_enable(1);
+
+System.Console.WriteLine("Test");
+
+var CommentStripperRegex = new Regex(@"(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)");
+
+static class InternalImportTest
+{
+	[DllImport("__Native")]
+	internal static extern void mono_trace_enable(int enable);
+}
