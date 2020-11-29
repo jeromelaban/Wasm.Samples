c3f8a6d75222697293080a69cb46a2253a7e4254
Jerome Laban <jerome.laban@nventive.com>
2020-11-28 23:26:06 -0500

    Add repro for https://github.com/dotnet/runtime/issues/45304

---
+++ Bug45304/Bug45304/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Test.Run();
+
+public class Test
+{
+	delegate void MyTargetHandler(object sender, MyTest args);
+
+	public static void Run()
+	{
+		MyTargetHandler handler = MyTarget;
+
+		var r = handler.DynamicInvoke(null, new MyTest());
+
+		Console.WriteLine($"Result: {r}");
+	}
+
+	public static void MyTarget(object sender, MyTest args)
+	{
+		Console.WriteLine("test");
+	}
+}
+
+public class MyTest : EventArgs
+{
+}
