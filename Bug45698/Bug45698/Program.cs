7cba901ecd16efb51744a27487111b3eba47132e
Jerome Laban <jerome.laban@nventive.com>
2020-12-07 15:04:49 -0500

    Add repro for https://github.com/dotnet/runtime/issues/45698

---
+++ Bug45698/Bug45698/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using System.Resources;
+
+var r = new ResourceManager("Bug44269.Resources.Strings", typeof(Bug44269.Resources.Strings).Assembly);
+Console.WriteLine(r.GetString("MyDefaultString"));
