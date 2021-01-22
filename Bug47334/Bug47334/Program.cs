abde54c5b70aa44a68208aeaea12fceee02c7643
Jerome Laban <jerome.laban@nventive.com>
2021-01-22 09:16:41 -0500

    feat: Sample for https://github.com/dotnet/runtime/issues/47334

---
+++ Bug47334/Bug47334/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing.Text;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+Console.WriteLine("Start");
+
+Run();
+
+async void Run()
+{
+	var client = new HttpClient();
+	var r = await client.GetAsync("http://invalid.url");
+
+	Console.WriteLine("test");
+}
---
+++ Bug47334/Bug47334/README.md
@@ -0,0 +1,5 @@
+﻿# Repro for issue https://github.com/dotnet/runtime/issues/47334
+
+Reproducing this requires .NET 5 RC1 or later, mono and ninja installed.
+
+Build with `dotnet build`
