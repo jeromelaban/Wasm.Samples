cf36ce42b24689f54c5a55d3a64d9372b6f851ee
Jerome Laban <jerome.laban@nventive.com>
2021-02-10 16:40:01 -0500

    Add repro for https://github.com/dotnet/runtime/issues/48126

---
+++ Bug48126/Bug48126/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing.Text;
+using System.Globalization;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Console.WriteLine("Start");
+
+const int outerCount = 1000000;
+int j = 0;
+
+// Nullable invocation
+var r1 = Stopwatch.StartNew();
+for (int i = 0; i < outerCount; i++)
+{
+	var r = GetMyNullable();
+
+	if(r != null)
+	{
+		j++;
+	}
+}
+r1.Stop();
+
+// Int32 invocation
+var r2 = Stopwatch.StartNew();
+for (int i = 0; i < outerCount; i++)
+{
+	var r = GetMyNonNullable();
+
+	if(r != 0)
+	{
+		j++;
+	}
+}
+r1.Stop();
+
+Console.WriteLine($"r1={r1.Elapsed} r2={r2.Elapsed}");
+
+int GetMyNonNullable()
+{
+	for (int i = 0; i < 1000000; i++)
+	{
+	}
+
+	return 0;
+}
+
+int? GetMyNullable()
+{
+	for (int i = 0; i < 1000000; i++)
+	{
+	}
+
+	return null;
+}
---
+++ Bug48126/Bug48126/README.md
@@ -0,0 +1,5 @@
+﻿# Repro for issue https://github.com/dotnet/runtime/issues/??
+
+Reproducing this requires .NET 5 RC1 or later, mono and ninja installed.
+
+Build with `dotnet build`
