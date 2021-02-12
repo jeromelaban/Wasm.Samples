512e5cb8e275e4ad8c21e870d36829c5a5cf94a7
Jerome Laban <jerome.laban@nventive.com>
2021-02-12 14:57:43 -0500

    repros for typecasts

--- BugPerfDelegate/BugPerfDelegate/Program.cs
+++ BugPerfDelegate/BugPerfDelegate/Program.cs
@@ -6,80 +6,76 @@ using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 // Console.WriteLine("Start");
 
 // const int outerCount = 1;
-const int outerCount = 1000000;
+const int outerCount = 10000;
 
-var disposable = new MyType();
-
-MyDelegate t1 = Registry.List;
-MyDelegate t2 = Function2;
+Test.MyClass m = new();
 
 // Nullable invocation
 var r1 = Stopwatch.StartNew();
 for (int i = 0; i < outerCount; i++)
 {
-	Invoke1(t1, disposable);
+	m.Content = i;
 }
 r1.Stop();
 
-// Int32 invocation
-var r2 = Stopwatch.StartNew();
-for (int i = 0; i < outerCount; i++)
-{
-	Invoke2(t2, disposable);
-}
-r2.Stop();
+//// Int32 invocation
+//var r2 = Stopwatch.StartNew();
+//for (int i = 0; i < outerCount; i++)
+//{
+//	Invoke2(t2, disposable);
+//}
+//r2.Stop();
 
-Console.WriteLine($"r1={r1.Elapsed} r2={r2.Elapsed}");
+Console.WriteLine($"r1={r1.Elapsed}");
 
-void Function2(object sender, EventArgs args)
+namespace Test
 {
-	((IDisposable)sender).Dispose();
-	for (int i = 0; i < 100000; i++)
+	public partial class MyClass : DependencyObject
 	{
-	}
-}
+		static int _value;
 
-void Invoke1(MyDelegate d, IDisposable d2)
-{
-	d.Invoke(d2, null);
-}
+		public object Content
+		{
+			get { return (object)GetValue(ContentProperty); }
+			set { SetValue(ContentProperty, value); }
+		}
 
-void Invoke2(MyDelegate d, IDisposable d2)
-{
-	d.Invoke(d2, null);
-}
+		// Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ContentProperty =
+			DependencyProperty.Register(
+				"Content",
+				typeof(object),
+				typeof(MyClass), 
+				new PropertyMetadata(
+					null,
+				 propertyChangedCallback: (s, e) => (s as MyClass).OnContentChanged(e.OldValue, e.NewValue)
+				 propertyChangedCallback: (s, e) => ((MyClass)s).OnContentChanged(e.OldValue, e.NewValue)
+				// propertyChangedCallback: OnStaticContentChanged
+				// propertyChangedCallback: (s, e) => OnStaticContentChanged(s, e)
+				)
+			);
 
-public class MyType : IDisposable
-{
-	static MyType()
-	{
-		Registry.Register((s, e) => {
-			((IDisposable)s).Dispose();
-			for (int i = 0; i < 100000; i++)
+		static void OnStaticContentChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			var end = (int)e.NewValue * 100;
+			for (int i = 0; i < end; i++)
 			{
+				_value = i;
 			}
-		});
-	}
+		}
 
-	public static void Init() { }
-
-	public virtual void Dispose()
-	{
-	}
-}
-
-class Registry
-{
-	public static MyDelegate List;
-
-	public static void Register(MyDelegate d)
-	{
-		List += d;
+		void OnContentChanged(object l, object r)
+		{
+			var end = (int)r * 100;
+			for (int i = 0; i < end; i++)
+			{
+				_value = i;
+			}
+		}
 	}
 }
-
-delegate void MyDelegate(object sender, EventArgs args);
