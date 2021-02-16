ff210b2ca43170cf2f564f8c387af81f241ea2e7
Jerome Laban <jerome.laban@nventive.com>
2021-02-16 12:25:36 -0500

    Add repro for https://github.com/dotnet/runtime/issues/48345

---
+++ Bug48345/Bug48345/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+static class Program
+{
+	static void Main(string[] args)
+	{
+		var r = MyMethod("42");
+
+		// Second invocation should show "value: 1 length:1", but shows "value: length:"
+		var r2 = MyMethod("1");
+	}
+
+	private static string MyMethod(string test)
+		=> WebAssembly.Runtime.InvokeJS($"Module.mono_bind_static_method(\"[Bug48345] Program:TestInterop\")(\"{test}\")");
+
+	public static string TestInterop(string value)
+	{
+		Console.WriteLine($"value: {value} length: {value?.Length}");
+		return "1";
+	}
+}
+
