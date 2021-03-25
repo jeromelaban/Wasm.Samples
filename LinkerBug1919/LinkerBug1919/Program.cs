6f1eefa664b6fcd797e6ce7b3e8a9ed7349cdbb7
Jerome Laban <jerome.laban@nventive.com>
2021-03-25 10:09:25 -0400

    Repro for https://github.com/mono/linker/issues/1919

---
+++ LinkerBug1919/LinkerBug1919/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uno.Wasm.Sample
+{
+	public static class Program
+	{
+		static void Main()
+		{
+			SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_sqlite3());
+
+			Console.WriteLine(typeof(Microsoft.EntityFrameworkCore.DbContext));
+
+			Console.WriteLine(SQLitePCL.raw.sqlite3_libversion_number().ToString());
+		}
+	}
+}
---
+++ LinkerBug1919/LinkerBug1919/README.md
@@ -0,0 +1,9 @@
+﻿# Repro for issue https://github.com/mono/linker/issues/1919
+
+Reproducing this requires .NET 5 RC1 or later, mono and ninja installed.
+
+Build with `dotnet build`
+
+The SDK and linker binaries are located in %TMP/dotnet-runtime-wasm-XXX. 
+
+The linker command line is provided when running `dotnet build /bl` in the `msbuild.binlog` file.
