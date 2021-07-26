a85ff3b2d67ee15494938d7ba9b8be358a56930b
Jerome Laban <jerome.laban@nventive.com>
2021-07-26 11:43:48 -0400

    chore: repro for https://github.com/dotnet/runtime/issues/56309

---
+++ Bug56309/Bug56309/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Uno.Wasm.Sample
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			TestTryCatch();
+		}
+
+		private static void TestTryCatch()
+		{
+			int InvokeMethod(int a)
+			{
+				try
+				{
+					return a++;
+				}
+				catch
+				{
+					Console.WriteLine("catch");
+					return 0;
+				}
+			}
+
+			var sw = Stopwatch.StartNew();
+			for (int i = 0; i < 10000000; i++)
+			{
+				InvokeMethod(i);
+			}
+			sw.Stop();
+			Console.WriteLine($"TestTryCatch: {sw.ElapsedMilliseconds}");
+		}
+	}
+}
