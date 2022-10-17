35a45dc080b599275e27c07183226040123a9693
Jerome Laban <jerome.laban@nventive.com>
2022-10-17 14:53:12 -0400

    Add repro for dotnet/runtime 77122

---
+++ AotIssueMismatch77122/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HttpClientRepro
+{
+public static class Program
+{
+    static async Task Main(string[] args)
+    {
+        await Fetch();
+    }
+
+    private static async Task Fetch()
+    {
+        try
+        {
+            var client = new System.Net.Http.HttpClient()
+            {
+                DefaultRequestHeaders = { { "origin", "WindowsCalculator" } }
+            };
+
+            var r = await client.GetAsync(new Uri("https://cors-anywhere.herokuapp.com/https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png"));
+            Console.WriteLine($"Got Response");
+            var c = await r.Content.ReadAsByteArrayAsync();
+            Console.WriteLine($"Got {c.Length} bytes");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failure: " + e);
+        }
+    }
+}
+}
