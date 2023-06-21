b6ff12de4af55d0b16aa1f19d93a7919c10ae6f6
Jerome Laban <jerome.laban@nventive.com>
2023-06-20 23:03:55 -0400

    feat: Profiled AOT bug https://github.com/dotnet/runtime/issues/87856

---
+++ BugProfiledAOT20220620/BugProfiledAOT20220620/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Uno.Wasm.Sample
+{
+	public static class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Directory.CreateDirectory("/tmp");
+			await File.WriteAllTextAsync("/tmp/test.txt", "test");
+			await File.ReadAllBytesAsync("/tmp/test.txt");
+		}
+	}
+
+	public static partial class Import
+	{
+		[System.Runtime.InteropServices.JavaScript.JSImport("globalThis.Test")]
+		public static partial void Test();
+	}
+}
---
+++ BugProfiledAOT20220620/BugProfiledAOT20220620/README.md
@@ -0,0 +1,7 @@
+﻿# Repro for issue https://github.com/dotnet/runtime/issues/50757
+
+Reproducing this requires .NET 5 RC1 or later, mono and ninja installed.
+
+Build with `dotnet build`
+
+The SDK files are located in %TMP/dotnet-runtime-wasm-XXX
